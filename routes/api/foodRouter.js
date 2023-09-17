113f4ffc0562b3c6171947d4f83dbed96d5c1e2b
jiml268 <jimlynch268@gmail.com>
2023-09-17 08:16:28 -0400

    getFoodByQuery end point created

--- routes/api/foodRouter.js
+++ routes/api/foodRouter.js
@@ -1,6 +1,8 @@
 const foodRouter = require('express').Router();
-// const {foods: ctrl } = require('../../controllers');
-// const { validation, ctrlWrapper, authorizeUser } = require('../../middlewares');
+ const {foods: ctrl } = require('../../controllers');
+ const { validation, ctrlWrapper, authorizeUser } = require('../../middlewares');
 
+foodRouter
+  .route("/food/query/:title").get(ctrlWrapper(ctrl.getFoodByQuery));
 
 module.exports = foodRouter;
--- services/index.js
+++ services/index.js
@@ -1,7 +1,9 @@
 const users = require("./users");
 const calories = require("./calories");
+const foods = require('./foods')
 
 module.exports = {
   calories,
   users,
+  foods
 };
