32f9669267888f46e6c96433956a5a7b15acfa79
Anison <barsikstd@gmail.com>
2023-09-20 22:43:10 -0500

    fixed delete food endpoint

--- controllers/users/userDeleteFood.js
+++ controllers/users/userDeleteFood.js
@@ -1,4 +1,4 @@
-const { user: service } = require('../../services');
+const { users: service } = require("../../services");
 
 const userDeleteFood = async (req, res) => {
   const result = await service.userDeleteFood(req);
@@ -9,40 +9,31 @@ const userDeleteFood = async (req, res) => {
       status: "Bad Request",
       code: 400,
       data: {
-        message: "Error with entered data, please try again."
-      }
-    })
+        message: "Error with entered data, please try again.",
+      },
+    });
     return;
   }
-      if (result === 401) {
-        res.status(401).json({
-          status: "Unauthorized",
-          code: 401,
-          data: {
-            message: "You are not authorized to continue, please login first."
-          }
-        })
-        return;
-  }
+
   if (result === 404) {
     res.status(404).json({
       status: "Product not found",
       code: 404,
       data: {
-        message: "There is no such product. Please check the product you want to delete."
-      }
-    })
+        message:
+          "There is no such product. Please check the product you want to delete.",
+      },
+    });
     return;
   }
-    
-    res.status(201).json({
-        status: "Success",
-        code: 201,
-      data: {
-          result,
-        }
-    });
-};
 
+  res.status(201).json({
+    status: "Success",
+    code: 201,
+    data: {
+      result,
+    },
+  });
+};
 
 module.exports = userDeleteFood;
--- services/users/userDeleteFood.js
+++ services/users/userDeleteFood.js
@@ -1,27 +1,23 @@
-const { Diary } = require('../../models');
+const { Diary } = require("../../models");
 
-const userDeleteFood = async (req, res) => {
-    const { date, productId } = req.body;
-    try {  
- const userId = req.session.userId;  
-    const diaryEntry = await Diary.findOne({
-      userId,
-      "entries.date": date,
-    });
-        if (diaryEntry) {
-            const foodItemIndex = diaryEntry.entries[0].foodItems.findIndex((item) => item.productId === productId);
-        
-            const result = await Diary.findOneAndDelete(foodItemIndex);
-            await diaryEntry.save();
-            return result;
-        }
+const userDeleteFood = async (req) => {
+  const { date, entryId } = req.body;
+  try {
+    const userId = req.session.userId;
+    const deleteFoodItem = await Diary.findOneAndUpdate(
+      { userId, "entries.date": date, "entries.foodItems._id": entryId },
+      { $pull: { "entries.$.foodItems": { _id: entryId } } },
+      { new: true }
+    );
+    if (!deleteFoodItem) {
+      return 404;
+    }
+    return deleteFoodItem;
   } catch (err) {
     // Throw an error with a meaningful message
     console.error(err);
     throw new Error("Error deleting food item: " + err.message);
   }
 };
-
-
 
 module.exports = userDeleteFood;
