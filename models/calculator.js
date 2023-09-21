475fdb4e1e14e51b7354b482e1e095ce7c30a440
StefieCaff <108297341+StefieCaff@users.noreply.github.com>
2023-09-20 19:28:53 -0700

    update calculator model
    make structure like the diary model

--- models/calculator.js
+++ models/calculator.js
@@ -1,49 +1,112 @@
-const { Schema, model } = require('mongoose');
-const { User } = require('./user');
+const { Schema, model} = require('mongoose')
 
+// Schema for the calculatorItem
+const calculatorItemSchema = new Schema(
+  {
+    height: {
+      type: Number,
+    },
+    weight: {
+      type: Number,
+    },
+    age: {
+      type: Number,
+    },
+    bloodType: {
+      type: Number,
+    },
+    currentWeight: {
+      type: Number,
+    },
+    desiredWeight: {
+      type: Number,
+    },
+    totalCalories: {
+      type: Number,
+    },
+    notAllowedFood: {
+      type: [],
+    },
+    measurementType: {
+      type: String,
+    },
+  },
+  { versionKey: false, timestamps: true },
+);
 
-const measurementSchema = new Schema({
+// Schema for a single calculator entry
+const calculatorEntrySchema = new Schema({
+  date: {
+    type: Date,
+    require: [true, "date is required"],
+  },
+  dailyRate: {
+    type: Number,
+    required: false
+  },
+  calculatorItems: [calculatorItemSchema], // An array of calculated items for the day
+},
+  { versionKey: false, timestamps: true },
+);
 
-  userId: {
-    type: Schema.Types.ObjectId,
-    ref: User,
-    required: [true, "userId is required"],
-  }, 
+// Schema for the entire calculator  (collection of calculator entries)
+const calculatorSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User", // Reference to the User model
+      required: true,
+    },
+    entries: [calculatorEntrySchema], // An array of calculator entries for the user
+  },
+  { versionKey: false, timestamps: true },
+);
+
+// const measurementSchema = new({
+
+//   userId: {
+//     type: Schema.Types.ObjectId,
+//     ref: User,
+//     required: [true, "userId is required"],
+//   }, 
   
-  height: {
-    type: Number,
+//   height: {
+//     type: Number,
     
-  },
-  weight: {
-    type: Number,
+//   },
+//   weight: {
+//     type: Number,
     
-  },
-  age: {
-    type: Number,
+//   },
+//   age: {
+//     type: Number,
     
-  },
-  bloodType: {
-    type: Number,
-  },
-  currentWeight: {
-    type: Number,
+//   },
+//   bloodType: {
+//     type: Number,
+//   },
+//   currentWeight: {
+//     type: Number,
     
-  },
-  desiredWeight: {
-    type: Number,
+//   },
+//   desiredWeight: {
+//     type: Number,
     
-  },
-  totalCalories: {
-    type: Number,
+//   },
+//   totalCalories: {
+//     type: Number,
     
-  },
-  notAllowedFood: {
-    type: [],
+//   },
+//   notAllowedFood: {
+//     type: [],
     
-  },
-});
+//   },
+//   measurementType: {
+//     type: String
+//   },
+// });
 
-const Calculator = model('calculator', measurementSchema);
+const Calculator = model('calculator', calculatorSchema);
 
 module.exports = {
   Calculator,
