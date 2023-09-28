5136f9e1f458a22ec5c4993892991d7c0edb7401
Jose Gonzales <jose.gonzales08@ymail.com>
2023-09-28 12:25:34 -0400

    [patch logout service]

--- services/auth/authLogout.js
+++ services/auth/authLogout.js
@@ -1,23 +1,18 @@
-require('dotenv').config();
+const { User } = require('../../models')
 
-const authLogout = (req, res) => {
+const authLogout = async (req, res) => {
     try {
-        // check if user is logged in
-        if(req.session.userToken) {
-            // if logged in, destroy session
-            req.session.destroy(() => {
-                res.json({message: "You have been logged out."});
-            });
-            // if not logged in, send message
-        } else {
-            res.json({message: "You are not logged in."});
+        const userLoggedIn = req.session.userId
+        const userExists = await User.findById(`${userLoggedIn}`) !== null;
+        if (userExists) {
+            req.session.destroy()
+            return 200
         }
+        return 404;
     } catch (err) {
-        // if error, send error
-        res.json({message: "There was an error with service."});
+        res.json({ message: "There was an error with service." });
         console.log(err);
     }
-    
 }
 
 module.exports = authLogout;
