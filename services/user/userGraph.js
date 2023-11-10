24e978a57cd897f5ab7aae070d798bd9cd1d841b
jiml268 <114753310+jiml268@users.noreply.github.com>
2023-11-10 11:52:27 -0500

    get data from weight graph

--- services/user/userGraph.js
+++ services/user/userGraph.js
@@ -1,18 +1,18 @@
-const { Diary } = require("../../models");
+const { Diary, Calculator } = require("../../models");
 
 const userGraph = async (req) => {
 
     try {
         const userId = req.user._id;
         const userDiary = await Diary.findOne({ userId });
-        const graphData = []
-        const graphDates = []
+        const grapDairyhData = []
+        const graphDiaryDates = []
         console.log('userDiary', userDiary)
         userDiary.entries.map((item) => {
 
 
-            graphDates.push(item.date)
-            graphData.push((Math.round(item.foodItems.reduce(function (prev, cur) {
+            graphDiaryDates.push(item.date)
+            grapDairyhData.push((Math.round(item.foodItems.reduce(function (prev, cur) {
                 return prev + cur.calories;
             }, 0) * 100) / 100))
 
@@ -21,11 +21,26 @@ const userGraph = async (req) => {
 
         })
 
+        const userCalc = await Calculator.findOne({ userId });
+        const graphCalcData = []
+        const graphCalcDates = []
+        userCalc.calculatorEntries.map((item) => {
+            console.log('item.date', item.date)
+            console.log('item.calculatorEntry[0].currentWeightLbs', item.calculatorEntry[0].currentWeightLbs)
+
+            graphCalcDates.push(item.date)
+            graphCalcData.push(item.calculatorEntry[0].currentWeightLbs)
+
+            return userCalc
+
+
+        })
+        console.log('graphCalcDates', graphCalcDates)
+        console.log('grapCalcData', graphCalcData)
 
 
 
-
-        return { graphData: graphData, graphDates: graphDates, code: 200 }
+        return { graphCalcDates: graphCalcDates, graphCalcData: graphCalcData, grapDairyhData: grapDairyhData, graphDiaryDates: graphDiaryDates, code: 200 }
 
     } catch (err) {
         console.log("Error geting  Archiving List", err);
