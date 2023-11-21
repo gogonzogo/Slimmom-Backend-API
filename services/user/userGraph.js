76125bc20d0fa086e5ff4a0467775a55d90c83c5
jiml268 <114753310+jiml268@users.noreply.github.com>
2023-11-21 08:26:06 -0500

    sort the dairy data for the graph

--- services/user/userGraph.js
+++ services/user/userGraph.js
@@ -4,36 +4,44 @@ const userGraph = async (req) => {
 
     try {
         const userId = req.user._id;
+        // const userDiary = await Diary.findOne({ userId });
         const userDiary = await Diary.findOne({ userId });
+        const diaryEntry = userDiary.entries.sort((a, b) => (a.date > b.date) ? 1 : ((b.date > a.date) ? -1 : 0))
+
         const grapDairyhData = []
         const graphDiaryDates = []
-        userDiary.entries.map((item) => {
+
+        if (diaryEntry.length > 0) {
+            diaryEntry.map((item) => {
 
 
-            graphDiaryDates.push(item.date)
-            grapDairyhData.push((Math.round(item.foodItems.reduce(function (prev, cur) {
-                return prev + cur.calories;
-            }, 0) * 100) / 100))
+                graphDiaryDates.push(item.date)
+                grapDairyhData.push((Math.round(item.foodItems.reduce(function (prev, cur) {
+                    return prev + cur.calories;
+                }, 0) * 100) / 100))
 
-            return userDiary
+                return userDiary
 
 
-        })
+            })
+        }
 
         const userCalc = await Calculator.findOne({ userId });
+
         const graphCalcData = []
         const graphCalcDates = []
-        userCalc.calculatorEntries.map((item) => {
+        if (userCalc) {
+            userCalc.calculatorEntries.map((item) => {
 
-            graphCalcDates.push(item.date)
-            graphCalcData.push(item.calculatorEntry[0].currentWeightLbs)
+                graphCalcDates.push(item.date)
+                graphCalcData.push(item.calculatorEntry[0].currentWeightLbs)
 
-            return userCalc
+                return userCalc
 
 
-        })
-
-        return { graphCalcDates: graphCalcDates, graphCalcData: graphCalcData, grapDairyhData: grapDairyhData, graphDiaryDates: graphDiaryDates, code: 200 }
+            })
+        }
+        return { diaryEntry: diaryEntry, graphCalcDates: graphCalcDates, graphCalcData: graphCalcData, grapDairyhData: grapDairyhData, graphDiaryDates: graphDiaryDates, code: 200 }
 
     } catch (err) {
         console.log(err);
